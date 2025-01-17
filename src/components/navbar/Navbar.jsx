6e5ae7dfa12aedfac71485f2fd84a3047c4337aa
hieu.nguyen12 <hieu.nguyen12@onemount.com>
2025-01-17 20:20:41 +0700

    update

--- src/components/navbar/Navbar.jsx
+++ src/components/navbar/Navbar.jsx
@@ -4,19 +4,21 @@ import MenuItem from './MenuItem'
 const Navbar = () => {
     return (
         <div style={{ backgroundColor: "#003680", padding: "12px 48px", color: "#ffff" }} >
-            <div className='flex align-center space-between'>
-                <p>Booking website</p>
-                <div>
-                    <button style={{ marginRight: "8px" }} className='btn-white'>Register</button>
-                    <button className='btn-white'>Login</button>
+            <div style={{ maxWidth: "1200px", margin: "0 auto" }}>
+                <div className='flex align-center space-between'>
+                    <p>Booking website</p>
+                    <div>
+                        <button style={{ marginRight: "8px" }} className='btn-white'>Register</button>
+                        <button className='btn-white'>Login</button>
+                    </div>
                 </div>
-            </div>
-            <div className='flex gap-12' style={{ marginTop: "24px" }}>
-                <MenuItem icon="fa fa-bed" label="Stays" isActive />
-                <MenuItem icon="fa fa-plane" label="Flights" />
-                <MenuItem icon="fa fa-car" label="Car rentals" />
-                <MenuItem icon="fa fa-bed" label="Attraction" />
-                <MenuItem icon="fa fa-taxi" label="Airport taxi" />
+                <div className='flex gap-12' style={{ marginTop: "24px" }}>
+                    <MenuItem icon="fa fa-bed" label="Stays" isActive />
+                    <MenuItem icon="fa fa-plane" label="Flights" />
+                    <MenuItem icon="fa fa-car" label="Car rentals" />
+                    <MenuItem icon="fa fa-bed" label="Attraction" />
+                    <MenuItem icon="fa fa-taxi" label="Airport taxi" />
+                </div>
             </div>
         </div>
     )
